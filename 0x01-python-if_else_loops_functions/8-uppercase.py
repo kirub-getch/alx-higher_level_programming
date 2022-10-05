b55174337effa765a35cfd5851542a03c92e65b2
kirub_getch <chaoskirub@gmail.com>
2022-10-05 19:23:20 +0000

    coverting in to uppercase letter

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(0, len(str)):
-        if 97 <= ord(str[i]) <= 123:
-            print('{}'.format(chr(ord(str[i]) - 32)), end='')
-        else:
-            print('{}'.format(str[i]), end='')
+    for i in str:
+        if 97 <= ord(i) <= 123:
+            i = chr(ord(i) - 32)
+        print('{}'.format(i), end='')
+    print('')

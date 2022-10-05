c39eec4bc5cd24566b63d95a2a087c7b917b50f2
kirub_getch <chaoskirub@gmail.com>
2022-10-05 16:55:07 +0000

    possible combo of 2 numbers

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for x in range(0, 100):
+    if x % 10 > x / 10 and not (x % 10 == x / 10) and not (x == 0):
+        if x == 89:
+            print('{:02d}'.format(x))
+        else:
+            print('{:02d}'.format(x), end=', ')
+    elif x == 0:
+        print('{:02d}'.format(x), end=', ')

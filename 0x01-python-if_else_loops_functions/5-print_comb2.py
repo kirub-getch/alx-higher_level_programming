408eb83f646afb3f09dd7b58330b814eed489591
kirub_getch <chaoskirub@gmail.com>
2022-10-05 14:03:58 +0000

    print numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
-for x in range(0, 100):
+n range(0, 100):
     if x == 99:
-        print('{}'.format(x))
+        print('{:2d}'.format(x))
     else:
-        print('{}'.format(x), end=', ')
+        print('{:02d}'.format(x), end=', ')

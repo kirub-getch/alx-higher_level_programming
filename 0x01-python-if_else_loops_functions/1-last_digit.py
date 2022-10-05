4998310c6e87c799dc34d0c5b854c8ae90481ef7
kirub_getch <chaoskirub@gmail.com>
2022-10-05 13:10:03 +0000

    printing last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 digit = abs(number) % 10
 if number < 0:
     digit: int = -1 * (10 - number % 10)
-print(f'Last digit of {number:d} is {digit:d}', end='')
+print(f'Last digit of {number:d} is {digit:d}', end=' ')
 if digit > 5:
     print(f'and is greater than 5')
 elif digit == 0:

64b35d64741eab8cc114e7603a9f12817923a0f9
kirub_getch <chaoskirub@gmail.com>
2022-10-05 18:11:26 +0000

    convert to upper case letter

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        if 97 <= ord(str[i]) <= 123:
+            print('{}'.format(chr(ord(str[i]) - 32)), end='')
+        else:
+            print('{}'.format(str[i]), end='')

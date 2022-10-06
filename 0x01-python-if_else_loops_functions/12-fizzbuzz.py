803df02eaef38cdd36cc411198ab16d08be9a012
kirub_getch <chaoskirub@gmail.com>
2022-10-06 10:50:10 +0000

    prining fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for x in range(1, 101):
-        print('{}'.format(x if not (x % 3 == 0 or x % 5 == 0) else ''), end=' ')
-        print('{}'.format('fizz' if (x % 3 == 0 and not(x % 5 == 0)) else ''), end=' ')
-        print('{}'.format('buzz' if (x % 5 == 0 and not(x % 3 == 0)) else ''), end=' ')
-        print('{}'.format('fizzbuzz' if (x % 5 == 0 and x % 3 == 0) else ''), end=' ')
+        if not (x % 3 == 0 or x % 5 == 0):
+            print('{} '.format(x), end='')
+        elif x % 3 == 0 and not x % 5 == 0:
+            print('{} '.format('fizz'), end='')
+        elif x % 5 == 0 and not x % 3 == 0:
+            print('{} '.format('buzz'), end='')
+        elif x % 5 == 0 and x % 3 == 0:
+            print('{} '.format('fizzbuzz'), end='')

0a704f1581bdad883eed029b8bb3177769629876
kirub-getch <kirub.getch@gmail.com>
2024-05-20 01:32:39 +0300

    modifed function use map and lambda

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 # 2-uniq_add.py
-# Brennan D Baraban <375@holbertonschool.com>
-
 
 def uniq_add(my_list=[]):
     """Add all unique integers in a list (once for each integer)."""
-    result = 0
-    for x in set(my_list):
-        result += x
-    return (result)
+    return sum(map(lambda x : int(x), set(my_list)))
